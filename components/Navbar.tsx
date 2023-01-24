860474fe38e27d7f8190c0389cc7b8eb464c58c8
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-01-24 19:38:37 +0530

    show details only on hover

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="h-14 bg-[#07080e] text-white flex items-center justify-between px-5 sticky top-0 left-0">
-      <div className="flex space-x-5">
+      <div className="flex space-x-5 items-center">
         <Link href="/">
           <div className="logo w-40 flex justify-center items-center">LOGO</div>
         </Link>
@@ -14,6 +14,9 @@ const Navbar = () => {
         />
       </div>
       <div className="flex space-x-5 items-center">
+        <Link href="/login">
+          <p>login</p>
+        </Link>
         <div className="avatar w-9 h-9 bg-red-500 rounded-sm  cursor-pointer"></div>
         <button className="w-28 h-9 bg-gradient-to-r from-green-200 to-green-500 rounded-sm text-black flex items-center justify-center hover:from-green-300 hover:to-green-600">
           Generate
--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import { BsHeart } from "react-icons/bs"
+import { BsHeart } from "react-icons/bs";
 
 const PostCard = ({ img }: any) => {
   return (
-    <div className="py-2 rounded-lg w-fit mx-auto cursor-pointer relative">
+    <div className="py-2 rounded-lg w-fit mx-auto cursor-pointer relative group">
       <Image
         src={`/images/${img}.png`}
         width={400}
@@ -11,14 +11,16 @@ const PostCard = ({ img }: any) => {
         alt=""
         className="rounded-lg hover:brightness-[.30]"
       />
-      <div className="top w-full absolute top-0 p-4 flex items-center justify-between">
-        <div className="flex items-center space-x-4">
-          <div className="avatar w-9 h-9 bg-blue-700 rounded-sm  cursor-pointer"></div>
-          <span className="font-semibold">RNK Kumbh</span>
+      <div className="top w-full absolute top-0 p-4 hidden group-hover:block">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-4">
+            <div className="avatar w-9 h-9 bg-blue-700 rounded-sm cursor-pointer"></div>
+            <span className="font-semibold">RNK Kumbh</span>
+          </div>
+          <BsHeart className="text-2xl" />
         </div>
-        <BsHeart className="text-2xl" />
       </div>
-      <p className="prompt absolute bottom-0 p-4">
+      <p className="prompt absolute bottom-0 p-4 hidden group-hover:block">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
         corporis nesciunt maxime! Ab dictaus.
       </p>
